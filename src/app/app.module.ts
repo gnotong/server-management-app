5783b77ff13ce3b04dbc413f897720e2d71a1a6d
notgabs <notgabs@computer.notgab.tech>
2023-03-16 01:34:26 +0100

    Spinner added globaly using interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA, ErrorHandler, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -12,10 +12,12 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { ConfirmationDialogComponent } from './components/dialogs/confirmation-dialog/confirmation-dialog.component';
 import { CreateServerDialogComponent } from './components/dialogs/create-server-dialog/create-server-dialog.component';
 import { ServerFormComponent } from './components/forms/server-form/server-form.component';
 import { CustomErrorHandler } from './errors/custom-error-handler.service';
+import { SpinnerInterceptor } from './shared/interceptors/spinner.interceptor';
 
 @NgModule({
   declarations: [
@@ -34,6 +36,7 @@ import { CustomErrorHandler } from './errors/custom-error-handler.service';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    NgxSpinnerModule.forRoot({type: 'ball-scale-multiple'}),
     MatInputModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
@@ -45,6 +48,11 @@ import { CustomErrorHandler } from './errors/custom-error-handler.service';
     {
       provide: ErrorHandler,
       useClass: CustomErrorHandler
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: SpinnerInterceptor,
+      multi: true
     }
   ],
   bootstrap: [AppComponent],
